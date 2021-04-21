c7a83d792cb176a3bf0da80c9304a8035fefb9b3
John Bunting <john.lee.bunting@gmail.com>
2021-04-21 11:57:23 -0500

    Noise attempts

--- _test.py
+++ _test.py
@@ -1,11 +1,37 @@
+"""Writes a 256x256 grayscale simplex noise texture file in pgm format
+(see http://netpbm.sourceforge.net/doc/pgm.html)
 """
-https://github.com/Mindwerks/worldengine
 
-import rawpy
-import imageio
+import sys
+from noise import pnoise2, snoise2
+from PIL import Image
 
-path = 'assets/heightmap.raw'
-with rawpy.imread(path) as raw:
-    rgb = raw.postprocess()
-imageio.imsave('default.tiff', rgb)
+
+if len(sys.argv) not in (2, 3) or '--help' in sys.argv or '-h' in sys.argv:
+    print('2dtexture.py FILE [OCTAVES]')
+    print()
+    print(__doc__)
+    raise SystemExit
+
 """
+f = open(sys.argv[1] + ".raw", 'wt')
+if len(sys.argv) > 2:
+    octaves = int(sys.argv[2])
+else:
+    octaves = 1
+freq = 16.0 * octaves
+f.write('P2\n')
+f.write('256 256\n')
+f.write('255\n')
+for y in range(256):
+    for x in range(256):
+        f.write("%s\n" % int(snoise2(x / freq, y / freq, octaves) * 127.0 + 128.0))
+f.close()
+"""
+
+arr = []
+for y in range(256):
+    for x in range(256):
+        f.write("%s\n" % int(snoise2(x / freq, y / freq, octaves) * 127.0 + 128.0))
+im = Image.fromarray(arr)
+im.show()
