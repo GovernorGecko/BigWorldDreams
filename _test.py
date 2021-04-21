33389d804313e3135d13fd6bf42360fcd7fa253e
John Bunting <john.lee.bunting@gmail.com>
2021-04-20 21:20:04 -0500

    Few things.

--- _test.py
+++ _test.py
@@ -1,3 +1,6 @@
+"""
+https://github.com/Mindwerks/worldengine
+
 import rawpy
 import imageio
 
@@ -5,3 +8,4 @@ path = 'assets/heightmap.raw'
 with rawpy.imread(path) as raw:
     rgb = raw.postprocess()
 imageio.imsave('default.tiff', rgb)
+"""
---
+++ src/chunk.py
@@ -0,0 +1,10 @@
+"""
+chunk
+
+A chunk is a x by x by x mesh.
+
+"""
+
+class Chunk:
+
+    def __init__(self, size, 
