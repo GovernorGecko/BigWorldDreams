b67a289b85c89a41c1fc8e05f5d9d5fb26f70b4d
John Bunting <john.lee.bunting@gmail.com>
2021-10-28 15:44:17 -0500

    Atlas work

--- src/atlas.py
+++ src/atlas.py
@@ -10,11 +10,21 @@ class Atlas():
 
     __slots__ = ["__base_path", "__images"]
 
-    def __init__(self, base_path=""):
+    def __init__(self, base_path="./", size=1024):
 
+        # Is our base path, valid?
         if not os.path.exists(base_path):
             raise ValueError("Please make sure path exists.")
+        # Size must be an int, and a power of 2
+        elif (
+            not isinstance(size, int) or
+            size <= 0
+        ):
+            raise ValueError(
+                "Size must be greater than 0, a power of 2, and an int."
+            )
 
+        # Set our base path
         self.__base_path = base_path
 
     def add_image(self, filename, path=""):
