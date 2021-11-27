c9ab31fd9a4c7afbeb3bddc41cf3fe80d5200acf
John Bunting <john.lee.bunting@gmail.com>
2021-11-27 15:49:01 -0600

    Added copying of files to folders for ease

--- _test.py
+++ _test.py
@@ -3,6 +3,7 @@
 """
 
 import os
+import shutil
 
 from src.atlas import Atlas
 from src.heightmap import create_heightmap
@@ -13,8 +14,8 @@ paths = {
     "results": "c:/repos/BigWorldDreams/results/",
 }
 
-area_size = 8
-tile_size = 4
+area_size = 4
+tile_size = 16
 
 atlas = Atlas("./files", size=1024)
 atlas.add_image("grass_top.png", "grass_top")
@@ -23,15 +24,22 @@ print(atlas.get_json())
 # print(atlas.get_texture_coords("test.png"))
 atlas.save("./")
 
-path_to_store = "./results"
-
-if not os.path.exists(path_to_store):
-    os.mkdir(path_to_store)
+if not os.path.exists(paths["results"]):
+    os.mkdir(paths["results"])
 else:
-    for file in os.scandir(path_to_store):
-        os.remove(file.path)
+    for root, dirs, files in os.walk(paths["results"]):
+        for f in files:
+            os.unlink(os.path.join(root, f))
+        for d in dirs:
+            shutil.rmtree(os.path.join(root, d))
 
-create_heightmap(area_size, tile_size, "tile", atlas, path_to_store)
+create_heightmap(
+    area_size,
+    tile_size,
+    "tile",
+    atlas,
+    paths["results"]
+)
 
 # blender_path = input("Blender Path: ")
 # script_path = input("Script Path: ")
@@ -43,3 +51,40 @@ stream = os.popen(
     f'-- "{paths["results"]}"'
 )
 output = stream.read()
+# print(output)
+
+
+def move_results_to_subfolder(file_type, subfolder):
+    """
+    Searches our results folder for the given file type to
+    the given subfolder.
+    """
+
+    files_to_move = os.listdir(paths["results"])
+    for file in files_to_move:
+        if file.endswith(file_type):
+            if not os.path.exists(
+                os.path.join(
+                    paths["results"], subfolder
+                )
+            ):
+                os.mkdir(
+                    os.path.join(
+                        paths["results"], subfolder
+                    )
+                )
+            shutil.move(
+                os.path.join(
+                    paths["results"], file
+                ),
+                os.path.join(
+                    paths["results"], subfolder, file
+                )
+            )
+
+
+move_results_to_subfolder("obj", "obj")
+move_results_to_subfolder("mtl", "obj")
+move_results_to_subfolder("fbx", "fbx")
+move_results_to_subfolder("json", "json")
+# shutil.copy(os.path.join(paths["results"], "atlas.png"), os.path.join(paths["results"], "obj", "atlas.png"))
